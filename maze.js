a6c90b91e3a5761e3600257cf423357feb68364f
stubby <pandorum117@gmail.com>
2022-09-02 14:46:01 -0600

    old model was removed,  divided in 3 components , add maze algorithm, add skin for player

---
+++ maze.js
@@ -0,0 +1,216 @@
+// iniciar canvas
+let maze = document.querySelector(".maze");
+let ctx = maze.getContext("2d");
+let generationComplete = false;
+let current;
+let goal;
+
+//calse para construir el laberinto
+class Maze {
+  constructor(size, rows, columns) {
+    this.size = size;
+    this.columns = columns;
+    this.rows = rows;
+    this.grid = [];
+    this.stack = [];
+  }
+
+  // asignar a la cuadricula la cantidad de filas y columans en la variable this.grid
+  setup() {
+    for (let r = 0; r < this.rows; r++) {
+      let row = [];
+      for (let c = 0; c < this.columns; c++) {
+        // Create a new instance of the Cell class for each element in the 2D array and push to the maze grid array
+        let cell = new Cell(r, c, this.grid, this.size);
+        row.push(cell);
+      }
+      this.grid.push(row);
+    }
+    // Set the starting grid
+    current = this.grid[0][0];
+    this.grid[this.rows - 1][this.columns - 1].goal = true;
+  }
+
+  // funcion draw para dibujar las celdas de la configuracion en el array de canvas
+  draw() {
+    maze.width = this.size;
+    maze.height = this.size;
+    maze.style.background = "black";
+    // establecer la primera celda como visitada
+    current.visited = true;
+    // Recorre la matriz de la cuadricula y se llama al metodo show para cada instancia de celda
+    for (let r = 0; r < this.rows; r++) {
+      for (let c = 0; c < this.columns; c++) {
+        let grid = this.grid;
+        grid[r][c].show(this.size, this.rows, this.columns);
+      }
+    }
+    // This function will assign the variable 'next' to random cell out of the current cells available neighbouting cells
+    let next = current.checkNeighbours();
+    // If there is a non visited neighbour cell
+    if (next) {
+      next.visited = true;
+      // Add the current cell to the stack for backtracking
+      this.stack.push(current);
+      // this function will highlight the current cell on the grid. The parameter columns is passed
+      // in order to set the size of the cell
+      current.highlight(this.columns);
+      // This function compares the current cell to the next cell and removes the relevant walls for each cell
+      current.removeWalls(current, next);
+      // Set the nect cell to the current cell
+      current = next;
+
+      // Else if there are no available neighbours start backtracking using the stack
+    } else if (this.stack.length > 0) {
+      let cell = this.stack.pop();
+      current = cell;
+      current.highlight(this.columns);
+    }
+    // If no more items in the stack then all cells have been visted and the function can be exited
+    if (this.stack.length === 0) {
+      generationComplete = true;
+      return;
+    }
+
+    // Recursively call the draw function. This will be called up until the stack is empty
+    window.requestAnimationFrame(() => {
+      this.draw();
+    });
+
+  }
+}
+
+class Cell {
+  // Constructor takes in the rowNum and colNum which will be used as coordinates to draw on the canvas.
+  constructor(rowNum, colNum, parentGrid, parentSize) {
+    this.rowNum = rowNum;
+    this.colNum = colNum;
+    this.visited = false;
+    this.walls = {
+      topWall: true,
+      rightWall: true,
+      bottomWall: true,
+      leftWall: true,
+    };
+    this.goal = false;
+    // parentGrid is passed in to enable the checkneighbours method.
+    // parentSize is passed in to set the size of each cell on the grid
+    this.parentGrid = parentGrid;
+    this.parentSize = parentSize;
+  }
+
+  checkNeighbours() {
+    let grid = this.parentGrid;
+    let row = this.rowNum;
+    let col = this.colNum;
+    let neighbours = [];
+
+    // The following lines push all available neighbours to the neighbours array
+    // undefined is returned where the index is out of bounds (edge cases)
+    let top = row !== 0 ? grid[row - 1][col] : undefined;
+    let right = col !== grid.length - 1 ? grid[row][col + 1] : undefined;
+    let bottom = row !== grid.length - 1 ? grid[row + 1][col] : undefined;
+    let left = col !== 0 ? grid[row][col - 1] : undefined;
+
+    // if the following are not 'undefined' then push them to the neighbours array
+    if (top && !top.visited) neighbours.push(top);
+    if (right && !right.visited) neighbours.push(right);
+    if (bottom && !bottom.visited) neighbours.push(bottom);
+    if (left && !left.visited) neighbours.push(left);
+
+    // la variable neighbours se elije aleatoriamente y luego retona el valor random
+    if (neighbours.length !== 0) {
+      let random = Math.floor(Math.random() * neighbours.length);
+      return neighbours[random];
+    } else {
+      return undefined;
+    }
+  }
+
+  // Funciones de dibujo  para cada celd, si el muro relevante se establece en verdadero en el constructor de celdas
+  drawTopWall(x, y, size, columns, rows) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + size / columns, y);
+    ctx.stroke();
+  }
+
+  drawRightWall(x, y, size, columns, rows) {
+    ctx.beginPath();
+    ctx.moveTo(x + size / columns, y);
+    ctx.lineTo(x + size / columns, y + size / rows);
+    ctx.stroke();
+  }
+
+  drawBottomWall(x, y, size, columns, rows) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + size / rows);
+    ctx.lineTo(x + size / columns, y + size / rows);
+    ctx.stroke();
+  }
+
+  drawLeftWall(x, y, size, columns, rows) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x, y + size / rows);
+    ctx.stroke();
+  }
+
+  // Resalta la celda actual en la cuadrícula. Las columnas se pasan una vez más para establecer el tamaño de la cuadrícula.
+  highlight(columns) {
+    // Sumas y restas agregadas para que la celda resaltada cubra las paredes
+    let x = (this.colNum * this.parentSize) / columns + 1;
+    let y = (this.rowNum * this.parentSize) / columns + 1;
+    //skin del jugador
+    ctx.fillStyle = "red";
+    ctx.fillRect(
+      x,
+      y,
+      this.parentSize / columns - 3,
+      this.parentSize / columns - 3
+    );
+  }
+
+  removeWalls(cell1, cell2) {
+    // se compara con dos celdas en el eje x
+    let x = cell1.colNum - cell2.colNum;
+    // Elimina las paredes relevantes si hay una diferente en el eje x
+    if (x === 1) {
+      cell1.walls.leftWall = false;
+      cell2.walls.rightWall = false;
+    } else if (x === -1) {
+      cell1.walls.rightWall = false;
+      cell2.walls.leftWall = false;
+    }
+    // compares to two cells on x axis
+    let y = cell1.rowNum - cell2.rowNum;
+    // Elimina las paredes relevantes si hay una diferente en el eje x
+    if (y === 1) {
+      cell1.walls.topWall = false;
+      cell2.walls.bottomWall = false;
+    } else if (y === -1) {
+      cell1.walls.bottomWall = false;
+      cell2.walls.topWall = false;
+    }
+  }
+
+  // Dibuja cada una de las celdas en el lienzo del laberinto.
+  show(size, rows, columns) {
+    let x = (this.colNum * size) / columns;
+    let y = (this.rowNum * size) / rows;
+    ctx.strokeStyle = "#ffffff";
+    ctx.fillStyle = "black";
+    ctx.lineWidth = 2;
+    if (this.walls.topWall) this.drawTopWall(x, y, size, columns, rows);
+    if (this.walls.rightWall) this.drawRightWall(x, y, size, columns, rows);
+    if (this.walls.bottomWall) this.drawBottomWall(x, y, size, columns, rows);
+    if (this.walls.leftWall) this.drawLeftWall(x, y, size, columns, rows);
+    if (this.visited) {
+      ctx.fillRect(x + 1, y + 1, size / columns - 2, size / rows - 2);
+    }
+    if (this.goal) {
+      ctx.fillStyle = "rgb(83, 247, 43)";
+      ctx.fillRect(x + 1, y + 1, size / columns - 2, size / rows - 2);
+    }
+  }
+}
